2be9eac10b4fe509c051334584784b68697bafe1
ASDAFF <asdaff.asad@yandex.ru>
2020-06-04 11:24:39 +0500

    up

--- lang/en/install/index.php
+++ lang/en/install/index.php
@@ -3,7 +3,7 @@
  * Copyright (c) 4/6/2020 Created By/Edited By ASDAFF asdaff.asad@yandex.ru
  */
 
-$MESS["CAPTCHA_INSTALL_NAME"] = "reCaptcha 2.0 Google Free";
+$MESS["CAPTCHA_INSTALL_NAME"] = "reCaptcha 2.0 Google";
 $MESS["CAPTCHA_INSTALL_DESCRIPTION"] = "https://www.google.com/recaptcha/intro/index.html";
 $MESS["CAPTCHA_INSTALL_TITLE"] = "Installing captcha reCaptcha 2.0";
 $MESS["CAPTCHA_UNINSTALL_TITLE"] = "Removing captcha reCaptcha 2.0";
